d0b39de7f2e26cb3e7e071d83fb510ab17a465e0
mcpat1993 <Patrick@Patricks-MBP-3.fios-router.home>
2016-03-27 11:08:23 -0400

    Committing all of my final work for the bonus project

--- template/bonus_one.js
+++ template/bonus_one.js
@@ -14,4 +14,83 @@
 (function() {
 	// Magic!
 	console.log('Keepin\'n it clean with an external script!');
+	$.getJSON("http://www.mattbowytz.com/simple_api.xml?data=all", function(data) {
+		console.log(data);
+	});
+
+	$.ajax({
+		url: "http://www.mattbowytz.com/simple_api.json",
+		data: {
+			data: "all"
+		},
+		method: 'GET',
+	}).done(function(data) {
+		console.log(data);
+	});
+	console.log();
 })();
+
+function find(searchText)
+{
+  //console.log("Search was called");
+  //document.getElementById('something').innerHTML = "changed";
+  if(searchText.length >= 1)
+  {
+  	$.ajax({
+      url: "http://www.mattbowytz.com/simple_api.json",
+      data: {
+        data: "all"
+      },
+      method: 'GET',
+    }).done(function(data) {
+      var output = '<ul>';
+      var result = [];
+     $.each(data["data"]["interests"], function(key, val){
+        result.push(val);
+      });
+      $.each(data["data"]["programming"], function(key, val){
+        result.push(val);
+      });
+      result.sort();
+      //console.log(result);
+      $.each(result, function(key, val){
+        if(val.search(searchText) != -1)
+        {
+          output+='<p><a  href=\'https://www.google.com/?gws_rd=ssl#q='+ val+'\'>'+val+'<a></p>';
+        }
+      });
+
+      /*for(var thing in data["data"]["interests"]){
+        output+= '<li>' + thing + '</li>';
+      }*/
+      output += '</ul>';
+      document.getElementById('suggestions').innerHTML = output;
+      $("p").hover(function(){
+          $(this).css("background-color", "grey");
+          }, function(){
+          $(this).css("background-color", "white");
+      });
+    });
+  }else
+  {
+    document.getElementById('suggestions').innerHTML = '';
+  }
+}
+
+function visit(targetLink)
+{
+  window.location.assign()
+}
+
+
+$('.flexsearch-input').keyup(function(){
+  var searchText = $('.flexsearch-input').val();
+  console.log(searchText);
+  find(searchText);
+});
+
+$("p").hover(function(){
+    $(this).css("background-color", "grey");
+    }, function(){
+    $(this).css("background-color", "white");
+});
